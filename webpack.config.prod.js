85a27d45b8fa93bb56492f0d809f24c834eadc9e
Krzysztof <krzychu222u@wp.pl>
2022-03-20 09:45:37 +0100

    styling typography on page/ configuring webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,36 +1,61 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: "./js/script.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss|sass$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
+    mode: "production",
+    entry: "./js/script.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    target: "web",
+    module: {
+        rules: [{
+                test: /\.scss|sass$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.svg$/,
+                type: "asset/inline",
+                resourceQuery: /inline/,
+            },
+            {
+                test: /\.jpg|png|svg$/,
+                use: "file-loader"
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]'
+                    }
+                }],
+                exclude: path.resolve(__dirname, './index.html')
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "landing-page",
+            template: "./index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "./images", to: "img" }
+            ],
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "landing-page",
-      template: "./index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "css/style.css",
-    }),
-  ],
 };
