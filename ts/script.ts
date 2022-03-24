cd260f377cb5cfa6470d9769a733e8bf5a13ae37
Krzysztof <krzychu222u@wp.pl>
2022-03-24 23:18:22 +0100

    styling menu and script for mobile menu

--- ts/script.ts
+++ ts/script.ts
@@ -1,1 +1,16 @@
 import "../scss/main.scss";
+import { MobileMenu } from "./modules/MobileMenu";
+
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const navigation:HTMLElement = document.querySelector('.nav');
+    const hamburger:HTMLElement = document.querySelector('.nav-hamburger');
+
+
+    if(navigation) {
+        const mobileMenu = new MobileMenu(hamburger, navigation);
+        mobileMenu.addListeners();
+    }
+})
